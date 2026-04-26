03c400284c4a1187f301101e9430be5988298a21
Daniel Agada <dangadaalfred258@gmail.com>
2026-04-26 22:37:13 +0100

    Hamburger & balance page

--- script.js
+++ script.js
@@ -4,7 +4,41 @@ const categoriesUl = document.getElementById("categories-ul");
 const inputSubmitBtn = document.getElementById("input-submit");
 const categoriesUlContainer = document.querySelector(".categories");
 const recordBtn = document.getElementById("record-btn");
+const homePage = document.getElementById("home-page");
+const balancePage = document.getElementById("balance-page");
 
+let balanceArr = [
+  {
+    portion: "Tithe",
+    amount: 0,
+    desination: "Church Acc",
+  },
+  {
+    portion: "Giving",
+    amount: 0,
+    desination: "Palmpay Acc",
+  },
+  {
+    portion: "Investment & Saving for Budgeted Needs",
+    amount: 0,
+    desination: "Moniepoint Acc",
+  },
+  {
+    portion: "House needs",
+    amount: 0,
+    desination: "Kuda Acc",
+  },
+  {
+    portion: "Data & Airtime",
+    amount: 0,
+    desination: "Smartcash Acc",
+  },
+  {
+    portion: "Transportation & Emergencies",
+    amount: 0,
+    desination: "Opay Acc",
+  },
+];
 let categoriesArr;
 let rules = [
   "10% Tithe from every income",
@@ -12,7 +46,15 @@ let rules = [
   "Never Spend outside budget when opay cannot comfortably take it",
 ];
 
+inputEl.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    inputSubmitBtn.click();
+  } else return;
+});
+
 inputSubmitBtn.addEventListener("click", () => {
+  if (inputEl.value.length === 0) return;
   categoriesArr = [
     {
       portion: "Tithe",
@@ -63,8 +105,77 @@ function displayPortions() {
   });
 }
 
+recordBtn.addEventListener("click", () => {
+  recordCategories();
+});
+
+function recordCategories() {
+  // categoriesArr.forEach((aItem) => {
+  //   const bItem = balanceArr.find((b) => b.portion === aItem.portion);
+  //   if (bItem) {
+  //     bItem.amount += aItem.amount; // Add amounts if names match
+  //   } else {
+  //     balanceArr.push({ ...aItem }); // Optional: add if it doesn't exist
+  //   }
+  // });
+
+  // console.log(balanceArr);
+
+  const bMap = new Map(balanceArr.map((item) => [item.portion, item]));
+  categoriesArr.forEach((aItem) => {
+    if (bMap.has(aItem.portion)) {
+      bMap.get(aItem.portion).amount += aItem.amount;
+    }
+  });
+
+  displayBalance();
+}
+
+function displayBalance() {
+  const portionCardContainer = document.getElementById("portion-cards");
+
+  const balance = balanceArr
+    .map(
+      (item) =>
+        `<section class="portion-card">
+        <p>${item.portion}</p>
+        <p>
+          <span>Total:</span> <span class="portion-amount">#${formatMoney(item.amount)}</span>
+        </p>
+      </section>`,
+    )
+    .join("");
+
+  portionCardContainer.innerHTML = balance;
+}
+
+function showHomePage() {
+  homePage.style.display = "block";
+  balancePage.style.display = "none";
+}
+
+function showBalance() {
+  homePage.style.display = "none";
+  balancePage.style.display = "block";
+  displayBalance();
+}
+
+//Format Money
 function formatMoney(amount) {
   return Number(amount).toLocaleString("en-US");
+}
+
+//Humburger Function
+function toggleMenu() {
+  let x = document.getElementById("myLinks");
+  let icon = document.querySelector(".icon");
+  if (x.className === "nav-links") {
+    x.className += " active";
+    icon.style.display = "none";
+  } else {
+    x.className = "nav-links";
+    icon.style.display = "block";
+  }
 }
 
 //Rules Display
